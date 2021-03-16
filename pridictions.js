c840aab416e782eceffb40c252090786082b901f
academyoftrading101 <79259557+academyoftrading101@users.noreply.github.com>
2021-03-16 18:18:07 +0530

    done for the day

---
+++ pridictions.js
@@ -0,0 +1,23 @@
+
+var month = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+]
+
+
+
+
+function loaded()
+{
+
+}
